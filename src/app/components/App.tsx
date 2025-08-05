e3590d0bd1dfcdae6060910cc3ea55b7940abc14
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-08-05 12:02:34 +0900

    feat: createPortal

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from '../../domain/hooks/services/useState'
 import { createPortal } from '../../platform/dom/createPortal'
 
-const modalRoot = document.getElementById('modal-root')
-
 export function App() {
   const [count, setCount] = useState<number>(0)
+
+  const modalRoot =
+    document.getElementById('modal-root') ??
+    (() => {
+      const div = document.createElement('div')
+      div.id = 'modal-root'
+      document.body.appendChild(div)
+      return div
+    })()
 
   return (
     <div id="app">
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      {createPortal(<div className="modal">I'm a modal</div>, modalRoot!)}
+      {modalRoot && createPortal(<div className="modal">I'm a modal</div>, modalRoot!)}
     </div>
   )
 }
--- src/platform/dom/createPortal.ts
+++ src/platform/dom/createPortal.ts
@@ -1,19 +1,10 @@
 import type { VNode } from '../../domain/vdom/model/vnode'
+import { render } from './render'
 
 export function createPortal(vnode: VNode, container: HTMLElement): VNode {
+  render(vnode, container)
   return {
-    type: () => {
-      if (container) {
-        container.innerHTML = ''
-        const renderedNode =
-          typeof vnode === 'string' ? document.createTextNode(String(vnode)) : null
-        if (renderedNode) container.appendChild(renderedNode)
-      } else {
-        console.warn('createPortal: container is null')
-      }
-
-      return vnode
-    },
-    props: {},
+    type: 'noscript',
+    props: { children: [] },
   }
 }
