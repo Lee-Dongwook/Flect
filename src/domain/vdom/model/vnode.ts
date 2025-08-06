7a08249d0b522635cd0ad6431103baef0028ee2d
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-08-06 11:20:52 +0900

    feat: change logic with fiber node from vnode

--- src/domain/vdom/model/vnode.ts
+++ src/domain/vdom/model/vnode.ts
@@ -1,7 +1,50 @@
 export interface VNode {
   type: string | Function
+  key?: string | null
+  ref?: any
   props: {
     [key: string]: any
     children?: Array<VNode | string | number | boolean | null>
+  }
+}
+
+export interface FiberNode {
+  type: any
+  key: null | string
+  ref: any
+  stateNode: any
+  return: FiberNode | null
+  child: FiberNode | null
+  sibling: FiberNode | null
+  alternate: FiberNode | null
+
+  pendingProps: any
+  memoizedProps: any
+  memoizedState: any
+
+  flags: number
+  index: number
+}
+
+export function createFiberFromVNode(vnode: VNode): FiberNode {
+  const { type, key = null, ref = null, props } = vnode
+
+  return {
+    type,
+    key,
+    ref,
+    stateNode: null,
+
+    return: null,
+    child: null,
+    sibling: null,
+    alternate: null,
+
+    pendingProps: props,
+    memoizedProps: null,
+    memoizedState: null,
+
+    flags: 0,
+    index: 0,
   }
 }
